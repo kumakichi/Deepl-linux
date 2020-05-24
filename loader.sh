bbed0e59a3ce4ee9bc6cb8c0373aad06b4d003f0
kumakichi <xyesan@gmail.com>
2020-05-24 21:58:39 +0800

    update doc

--- loader.sh
+++ loader.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+WIDTH=800
+HEIGHT=600
+LISTEN_PORT=9331
+
 SCRIPT_PATH=${BASH_SOURCE[0]}
 if [ -L "${SCRIPT_PATH}" ]
 then
     SCRIPT_PATH=$(readlink $SCRIPT_PATH)
 fi
-SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
+SCRIPT_DIR="$(cd "$( dirname "${SCRIPT_PATH}" )" && pwd)"
 
-"$SCRIPT_DIR/Deepl-translator-linux"
+$("$SCRIPT_DIR/Deepl-translator-linux" -w ${WIDTH} -h ${HEIGHT} -p ${LISTEN_PORT})
 
 xdotool search --desktop 0 Deepl-translator-linux windowactivate
